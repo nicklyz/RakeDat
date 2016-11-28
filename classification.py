b8046dc618ef5433c4d9332fce759a73000eb5d8
Yanzhe Liu <nickliuyanzhe@gmail.com>
2016-11-28 13:19:22 -0800

    Made it into two separate files

--- classification.py
+++ classification.py
@@ -1,8 +1,4 @@
 import json
-import pandas as pd
-import numpy as np
-from sklearn.feature_extraction.text import HashingVectorizer
-from time import time
 
 """
 An example of a dish
@@ -22,53 +18,67 @@ An example of a dish
   ]
 }
 """
-# pandas to load data
-traindf = pd.read_json('data/train.json')
-testdf = pd.read_json('data/test.json')
 
-# use scikit-learn's bag of words tool
-print("Extracting features from the training data using a sparse vectorizer")
-vectorizer = HashingVectorizer(analyzer = "word",   \
-                             tokenizer = None,    \
-                             preprocessor = None, \
-                             stop_words = None,   \
-                             n_features = 5000)
+# preprocess data
+with open('data/train.json') as f:
+    trainData = json.load(f)
+    allCuisines = {} # map from cuisine name to number
+    allCuisinesList = []
+    allIngredients = {} # map from ingredient name to number
+    cntIngr = 0 # counter for ingredients
+    for dish in trainData:
+        # processing cuisine
+        cuisine = dish['cuisine']
+        if cuisine not in allCuisines:
+            allCuisines[cuisine] = len(allCuisinesList)
+            allCuisinesList.append(cuisine)
+        dish['cuisine'] = allCuisines[cuisine]
+        # processing ingredients
+        ingredients = dish['ingredients']
+        for idx, ingr in enumerate(ingredients):
+            if ingr not in allIngredients:
+                allIngredients[ingr] = cntIngr
+                cntIngr += 1
+            ingredients[idx] = allIngredients[ingr]
+    trainDataMatrix = []
+    for dish in trainData:
+        row = [0] * cntIngr
+        for ingr in dish['ingredients']:
+            row[ingr] = 1
+        trainDataMatrix.append(row)
 
-print "Creating the bag of words features for training data..."
-t0 = time()
-traindf['ingredients_clean_string'] = [' '.join(ingred).strip().lower() for ingred in traindf['ingredients']]
-train_data_features = vectorizer.fit_transform(traindf['ingredients_clean_string']).todense()
-duration = time() - t0
-print("done in %fs" % duration)
-print("n_samples: %d, n_features: %d " % train_data_features.shape)
-
-print "Creating the bag of words features for test data..."
-t0 = time()
-testdf['ingredients_clean_string'] = [' '.join(ingred).strip().lower() for ingred in testdf['ingredients']]
-test_data_features = vectorizer.transform(testdf['ingredients_clean_string']).todense()
-duration = time() - t0
-print("done in %fs" % duration)
-print("n_samples: %d, n_features: %d " % test_data_features.shape)
+with open('data/test.json') as f:
+    testData = json.load(f)
+    testDataMatrix = []
+    for dish in testData:
+        ingredients = [ingr for ingr in dish['ingredients'] if ingr in allIngredients]
+        row = [0] * cntIngr
+        for ingr in ingredients:
+            row[allIngredients[ingr]] = 1
+        testDataMatrix.append(row)
 
 # Plug in algorithm here
 #from sklearn.naive_bayes import GaussianNB # 34.4%
 #clf = GaussianNB()
-from sklearn.tree import DecisionTreeClassifier # 63.053%
+from sklearn.tree import DecisionTreeClassifier # 61.9%
 clf = DecisionTreeClassifier()
 # from sklearn.svm import SVC
 # clf = SVC()
 print 'Starting training'
-t0 = time()
-clf.fit(train_data_features, traindf['cuisine'])
-duration = time() - t0
-print("done in %fs" % duration)
-
+clf.fit(trainDataMatrix, [dish['cuisine'] for dish in trainData])
 print 'Starting predicting'
-t0 = time()
-result = clf.predict(test_data_features)
-duration = time() - t0
-print("done in %fs" % duration)
+result = map(lambda i: allCuisinesList[i], clf.predict(testDataMatrix))
 
-print 'Outputing result'
+
+import pandas as pd
+testdf = pd.read_json('data/test.json')
 output = pd.DataFrame( data={"id":testdf["id"], "cuisine":result} )
 output.to_csv( "data/submission.csv", index=False, quoting=3 )
+# Output in csv for submission on Kaggle
+# import csv
+# with open('submission.csv', 'wb') as f:
+#     writer = csv.writer(f)
+#     writer.writerow(('id', 'cuisine'))
+#     for i, ingr in zip([dish['id'] for dish in testData], result):
+#         writer.writerow((i, ingr))
+#
